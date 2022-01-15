d88c4d6f9a9c66e1fa8fd0242360092af2b38ec9
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-15 16:51:57 +0100

    Added random word generator

---
+++ source/generator.js
@@ -0,0 +1,9 @@
+import wordlist from '../wordlist/list.json'
+
+function randint(ceiling) {
+    return Math.floor( Math.random() * ceiling )
+}
+
+export function random() {
+    return wordlist[ randint(wordlist.length) ]
+}
