9b4b3f6f1389c4d6cba6ae950d6daf9707d30c04
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-15 17:55:37 +0100

    Extracted WordElement class to module

--- source/game.js
+++ source/game.js
@@ -1,55 +1,6 @@
 import * as keyboard from './keyboard'
 import * as generator from './generator'
-
-// WordElement class
-class WordElement {
-    selectedClass = 'current'
-
-    container = null
-    letters = null
-
-    constructor(wrapper) {
-        if (wrapper != null) {
-            this.generate(wrapper)
-        }
-    }
-
-    generate = (wrapper) => {
-        //Create container
-        this.container = document.createElement('div')
-        this.container.classList.add('word-line')
-
-        //Create letters
-        this.letters = []
-        for (let i = 0; i < 5; i++) {
-            let letter = document.createElement('div')
-            letter.classList.add('letter')
-            letter.innerText = ''
-
-            this.container.appendChild(letter)
-            this.letters.push(letter)
-        }
-
-        //Add to wrapper
-        wrapper.appendChild(this.container)
-    }
-
-    select = () => {
-        this.container.classList.add(this.selectedClass)
-    }
-
-    deselect = () => {
-        this.container.classList.remove(this.selectedClass)
-    }
-
-    addLetter = (letter, index) => {
-        this.letters[index].innerText = letter.toUpperCase()
-    }
-
-    removeLetter = (index) => {
-        this.letters[index].innerText = ''
-    }
-}
+import WordElement from './word-element'
 
 // Loading
 
