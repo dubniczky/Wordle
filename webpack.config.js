1e86f6eb629a056e5082eb04028cc03b6c280b78
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-15 15:07:06 +0100

    Added copy plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+const CopyPlugin = require("copy-webpack-plugin")
+
 module.exports = {
     entry: './source/main.js',
     output: {
         filename: 'dist/bundle.js'
     },
-    mode: 'development'
+    mode: 'development',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "page", to: "dist" },
+            ],
+        }),
+    ]
 }
