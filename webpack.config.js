b303dd1fe8314be1a12d5795e5d47b775d207cb0
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-15 18:53:56 +0100

    Split css into modules, added bundling script

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const path = require('path')
+const fs = require('fs')
+
 const CopyPlugin = require("copy-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './source/main.js',
+    entry: {
+        'bundle.js': [
+            path.resolve(__dirname, './source/main.js')
+        ],
+    },
     output: {
-        filename: 'bundle.js'
+        filename: '[name]',
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'source-map',
     plugins: [
@@ -25,5 +34,20 @@ module.exports = {
                 parallel: true,
             })
         ],
-    },
+    }
 }
+
+// Bundle CSS files
+function bundleCSS(folder, target) {
+    let files = fs.readdirSync(folder)
+    let data = ''
+
+    for (let f of files)
+    {
+        data += fs.readFileSync(path.resolve(folder, f), 'utf-8') + '\n'
+    }
+
+    fs.writeFileSync(target, data)
+}
+
+bundleCSS('./style/', './dist/bundle.css')
