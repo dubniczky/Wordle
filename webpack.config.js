2cd9e20f0d96e094da4ca5a55a21808c30b108ee
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-15 15:16:28 +0100

    Fixed a bug with webpack config paths

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const CopyPlugin = require("copy-webpack-plugin")
 module.exports = {
     entry: './source/main.js',
     output: {
-        filename: 'dist/bundle.js'
+        filename: 'bundle.js'
     },
     mode: 'development',
     plugins: [
         new CopyPlugin({
             patterns: [
-                { from: "page", to: "dist" },
+                { from: "page", to: "." },
             ],
         }),
     ]
