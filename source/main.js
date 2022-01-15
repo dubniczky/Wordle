d7faec16ae7f7526c0d1186a70acddee5454d498
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-15 15:40:02 +0100

    Created word object

--- source/main.js
+++ source/main.js
@@ -1,1 +1,16 @@
-console.log('yay')
+// Load components
+
+class Word {
+    container = null
+    letters = null
+
+    constructor(container) {
+        this.container = container
+        this.letters = container.children
+    }
+}
+
+const wrapper = document.getElementById('wrapper')
+
+let w1 = new Word(wrapper.children[0])
+console.log(w1)
