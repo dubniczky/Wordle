115a1fad6d9fc8c7b8bb4bfc727edb02fc02624d
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-26 19:05:35 +0100

    Removed terser compressor from development version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 const CopyPlugin = require("copy-webpack-plugin")
-const TerserPlugin = require("terser-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin").default
 
 module.exports = {
@@ -18,18 +17,6 @@ module.exports = {
         }),
         new MiniCssExtractPlugin()
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                minify: TerserPlugin.uglifyJsMinify,
-                terserOptions: {
-                    compress: true
-                },
-                parallel: true,
-            })
-        ],
-    },
     module: {
         rules: [
             {
